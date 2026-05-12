d4cf4f2639674fc5cf32ed78809e67896a1f98b3
Jahangir <c.orucov2005@gmail.com>
2026-05-12 13:21:54 +0300

    chore: add SQLite JDBC dependency and initialize DatabaseManager

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.nixenthusiast.ce316project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens org.nixenthusiast.ce316project to javafx.fxml;
