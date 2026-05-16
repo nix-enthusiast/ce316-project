aa181ad58905745f5f4873908f6fa0934edb4270
mburakaras <mehmet.aras@std.izmirekonomi.edu.tr>
2026-05-16 19:11:38 +0300

    ui: implement main application window shell and navigation menu bar

---
+++ src/main/java/com/iae/controller/MainController.java
@@ -0,0 +1,62 @@
+package com.iae.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+
+public class MainController {
+
+    private static MainController instance;
+
+    @FXML
+    private BorderPane mainContainer;
+
+    public MainController() {
+        instance = this;
+    }
+
+    public static MainController getInstance() {
+        return instance;
+    }
+
+    @FXML
+    public void initialize() {
+        loadView("/com/iae/controller/dashboard-view.fxml");
+    }
+
+    @FXML
+    void showDashboard(ActionEvent event) {
+        loadView("/com/iae/controller/dashboard-view.fxml");
+    }
+
+    @FXML
+    void showConfigurationManager(ActionEvent event) {
+        loadView("/com/iae/controller/configuration-view.fxml");
+    }
+
+    @FXML
+    void showHelpManual(ActionEvent event) {
+        loadView("/com/iae/controller/help-view.fxml");
+    }
+
+    @FXML
+    void exitApplication(ActionEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void loadView(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent view = loader.load();
+            mainContainer.setCenter(view);
+        } catch (IOException e) {
+            System.err.println("Error loading view: " + fxmlPath);
+            e.printStackTrace();
+        }
+    }
+}
